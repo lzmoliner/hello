15d9a0121729e5e1e4630c4145b8bdff03263546
lzmoliner <lzmoliner@gmail.com>
2023-03-30 14:54:55 -0300

    Solving bugs in create_person method

--- hello.py
+++ hello.py
@@ -9,15 +9,15 @@ def create_person():
     person = {}
     print('Hello, welcome M&L Company interview!')
     print('Please enter the following data about you')
-    person.name = input('Your name: ')
-    person.age = int(input('Your age: '))
-    person.opcupation = input('Your ocupation: ')
-    person.other_information = input('Other information about you')
+    person['name'] = input('Your name: ')
+    person['age'] = int(input('Your age: '))
+    person['ocupation'] = input('Your ocupation: ')
+    person['other_information'] = input('Other information about you: ')
     return person
 
 def main():
     person = create_person()
-    hello(person.name)
+    hello(person['name'])
    
 if __name__ == "__main__":
     main()
